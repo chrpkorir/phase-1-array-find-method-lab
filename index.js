4c8b362c2a3c48d5b642f582c2f9b3a7f025ac65
[chrpkorir] <faythkoko@gmail.com>
2024-12-12 12:02:42 +0300

    Practised on find()

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const records = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+function superbowlWin(arr){
+    //use find() to get the first object where result = "W"
+    const recordFound = arr.find(record => record.result === "W")
+    // if record is found, return the year otherwise return undefined
+    return recordFound? recordFound.year : undefined
+}
+console.log(superbowlWin(records))
